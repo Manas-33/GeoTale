1a9c15bee403eae1147f3124e55ecedb43ccab12
Manas Dalvi <dalvimanas33@gmail.com>
2024-04-05 19:45:45 +0530

    LG basic functions

---
+++ lib/connections/lg.dart
@@ -0,0 +1,110 @@
+import 'dart:io';
+
+import 'package:dartssh2/dartssh2.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LGConnection {
+  SSHClient? _client;
+  late String _host;
+  late String _port;
+  late String _username;
+  late String _passwordOrKey;
+  late String _numberOfRigs;
+
+  Future<void> initConnectionDetails() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    _host = prefs.getString('ipAddress') ?? '192.168.56.101';
+    _port = prefs.getString('sshPort') ?? '22';
+    _username = prefs.getString('username') ?? 'lg';
+    _passwordOrKey = prefs.getString('password') ?? 'lg';
+    _numberOfRigs = prefs.getString('numberOfRigs') ?? '3';
+  }
+
+  Future<bool?> connectToLG() async {
+    await initConnectionDetails();
+    try {
+      _client = SSHClient(await SSHSocket.connect(_host, int.parse(_port)),
+          username: _username, onPasswordRequest: () => _passwordOrKey);
+      return true;
+    } on SocketException catch (e) {
+      print('Failed to connect: $e');
+      return false;
+    }
+  }
+
+  rebootLG() async {
+    try {
+      connectToLG();
+
+      for (int i = int.parse(_numberOfRigs); i > 0; i--) {
+        await _client!.execute(
+            'sshpass -p $_passwordOrKey ssh -t lg$i "echo $_passwordOrKey | sudo -S reboot"');
+      }
+      return null;
+    } catch (e) {
+      print("An error occurred while executing the command: $e");
+      return null;
+    }
+  }
+
+  searchPlace(String placeName) async {
+    try {
+      connectToLG();
+      final execResult =
+          await _client?.execute('echo "search=$placeName" >/tmp/query.txt');
+      return execResult;
+    } catch (e) {
+      print('An error occurred while executing the command: $e');
+      return null;
+    }
+  }
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  buildOrbit(String content) async {
+    String localPath = await _localPath;
+    File localFile = File('$localPath/Orbit.kml');
+    localFile.writeAsString(content);
+    try {
+      connectToLG();
+      await _client?.run("echo '$content' > /var/www/html/Orbit.kml");
+      await _client!.execute(
+          "echo '\nhttp://lg1:81/Orbit.kml' >> /var/www/html/kmls.txt");
+      return await _client!.execute('echo "playtour=Orbit" > /tmp/query.txt');
+    } catch (e) {
+      print('Error in building orbit');
+      return Future.error(e);
+    }
+  }
+
+  startOrbit() async {
+    try {
+      return await _client!.execute('echo "playtour=Orbit" > /tmp/query.txt');
+    } catch (e) {
+      print('Could not connect to host LG');
+      return Future.error(e);
+    }
+  }
+
+  stopOrbit() async {
+    try {
+      return await _client!.execute('echo "exittour=true" > /tmp/query.txt');
+    } catch (e) {
+      print('Could not connect to host LG');
+      return Future.error(e);
+    }
+  }
+
+  cleanOrbit() async {
+    try {
+      return await _client!.execute('echo "" > /tmp/query.txt');
+    } catch (e) {
+      print('Could not connect to host LG');
+      return Future.error(e);
+    }
+  }
+}
