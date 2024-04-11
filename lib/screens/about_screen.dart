e5a806ee22059ed56d7a5e139e680c5ee09866eb
Manas Dalvi <dalvimanas33@gmail.com>
2024-04-12 02:53:08 +0530

    about screen ui

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -2,6 +2,8 @@ import 'package:ai_app/components/connection_flag.dart';
 import 'package:ai_app/components/drawer.dart';
 import 'package:ai_app/connections/lg.dart';
 import 'package:ai_app/constants.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -109,28 +111,197 @@ class _AboutScreenState extends State<AboutScreen> {
             children: [
               SingleChildScrollView(
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+                    Text(
+                      "Explore and Discover with GeoTale",
+                      style:
+                          googleTextStyle(30.sp, FontWeight.w500, Colors.white),
+                    ),
+                    SizedBox(
+                      height: 40.h,
+                    ),
                     Container(
-                      width: size.width*0.85,
-                      child: Text("""This application leverages the capabilities of Gemini, a large language model, in conjunction with Google Earth to create an immersive interactive storytelling experience. By seamlessly integrating the power of natural language generation with the vast geographical knowledge of Google Earth, this application unlocks a world of possibilities for creative exploration and education.
-                    
-                    Here's an elaboration of the process:
-                    Location Selection:
-                    Users interact with the Liquid Galaxy globe, selecting a specific location that piques their interest.
-                    Story Generation:
-                    Once a location is chosen, Gemini's language model springs into action. Drawing on its vast knowledge and understanding of the world's diverse regions, cultures, and histories, Gemini crafts a unique short story tailored to the selected location.
-                    Story Presentation:
-                    The generated story is not merely displayed as text. Instead, it is skillfully interwoven with the location on the Liquid Galaxy screen, creating a captivating visual and narrative experience. The story unfolds as users explore the selected region, with text and visuals harmoniously complementing each other.
-                    Interactive Exploration:
-                    The application encourages users to actively explore the globe, discovering new stories and perspectives from various corners of the world. As they navigate from one location to another, Gemini continues to generate fresh narratives, ensuring a continuous and engaging storytelling journey.
-                    Educational Value:
-                    Beyond entertainment, this application holds educational potential. It presents a unique way to learn about different cultures, historical events, and geographical landmarks. By weaving stories around real-world locations, it fosters a deeper understanding and appreciation of the world's diverse heritage.
-                    This interactive storytelling experience aims to ignite curiosity, inspire creativity, and provide a novel way to engage with the wonders of our planet. We invite you to embark on this enchanting adventure and discover the limitless possibilities that lie within the fusion of language and geography.
-                    
-                    """,style: googleTextStyle(20.sp, FontWeight.w500, Colors.cyan),),
-                    )
+                      width: size.width * 0.8,
+                      child: Text(
+                        "GeoTale is your personal guide to discovering the world's cities and their fascinating tales. With just a simple search, you can explore a city and uncover its hidden gems. ",
+                        style: googleTextStyle(
+                            22.sp, FontWeight.w500, Colors.cyan),
+                        overflow: TextOverflow.clip,
+                        textAlign: TextAlign.justify,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40.h,
+                    ),
+                    Container(
+                      height: size.height * 0.45,
+                      width: size.width * 0.4,
+                      child: CarouselSlider(
+                          items: [
+                            Container(
+                              decoration: BoxDecoration(
+                                  color: secondColor.withOpacity(0.5),
+                                  borderRadius:
+                                      BorderRadiusDirectional.circular(10)),
+                              child: Container(
+                                width: size.width * 0.35,
+                                padding: EdgeInsets.all(30),
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      "Technology Used",
+                                      style: googleTextStyle(
+                                          23.sp, FontWeight.w700, Colors.white),
+                                    ),
+                                    SizedBox(
+                                      height: 20.h,
+                                    ),
+                                    Container(
+                                      width: size.width * 0.35,
+                                      child: Text(
+                                        "This app utilizes state-of-the-art AI technology to bring each destination to life. Gemini AI generates detailed information about the city and its points of interest, while Deepgram AI provides captivating narration for your journey. ",
+                                        style: googleTextStyle(17.sp,
+                                            FontWeight.w500, Colors.cyan),
+                                        overflow: TextOverflow.clip,
+                                        textAlign: TextAlign.justify,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 2.h,
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                  color: secondColor.withOpacity(0.5),
+                                  borderRadius:
+                                      BorderRadiusDirectional.circular(10)),
+                              child: Container(
+                                width: size.width * 0.35,
+                                padding: EdgeInsets.all(30),
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      "Orbiting Funtions",
+                                      style: googleTextStyle(
+                                          23.sp, FontWeight.w700, Colors.white),
+                                    ),
+                                    SizedBox(
+                                      height: 20.h,
+                                    ),
+                                    Container(
+                                      width: size.width * 0.35,
+                                      child: Text(
+                                        "One of the key features of GeoTale is the ability to create your own orbit. This immersive experience takes you on a tour of the city's POIs, with informative flashcards and narration to guide you. It's like having your own personal rig to explore the world.",
+                                        style: googleTextStyle(17.sp,
+                                            FontWeight.w500, Colors.cyan),
+                                        overflow: TextOverflow.clip,
+                                        textAlign: TextAlign.justify,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 2.h,
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                  color: secondColor.withOpacity(0.5),
+                                  borderRadius:
+                                      BorderRadiusDirectional.circular(10)),
+                              child: Container(
+                                width: size.width * 0.35,
+                                padding: EdgeInsets.all(30),
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      "Awesome Storytelling",
+                                      style: googleTextStyle(
+                                          23.sp, FontWeight.w700, Colors.white),
+                                    ),
+                                    SizedBox(
+                                      height: 20.h,
+                                    ),
+                                    Container(
+                                      width: size.width * 0.35,
+                                      child: Text(
+                                        "But that's not all. GeoTale also offers a unique storytelling feature. Choose from suggested themes and let Gemini AI weave a captivating story about your chosen city. It's a whole new way to learn about a place and its culture. ",
+                                        style: googleTextStyle(17.sp,
+                                            FontWeight.w500, Colors.cyan),
+                                        overflow: TextOverflow.clip,
+                                        textAlign: TextAlign.justify,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 2.h,
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                  color: secondColor.withOpacity(0.5),
+                                  borderRadius:
+                                      BorderRadiusDirectional.circular(10)),
+                              child: Container(
+                                width: size.width * 0.35,
+                                padding: EdgeInsets.all(30),
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      "Educative Experience",
+                                      style: googleTextStyle(
+                                          23.sp, FontWeight.w700, Colors.white),
+                                    ),
+                                    SizedBox(
+                                      height: 20.h,
+                                    ),
+                                    Container(
+                                      width: size.width * 0.35,
+                                      child: Text(
+                                        "GeoTale, aims to inspire curiosity and provide a unique way to explore the world. It's perfect for those who want to discover new places, learn about different cultures, or simply enjoy a captivating story.",
+                                        style: googleTextStyle(17.sp,
+                                            FontWeight.w500, Colors.cyan),
+                                        overflow: TextOverflow.clip,
+                                        textAlign: TextAlign.justify,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 2.h,
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                          options: CarouselOptions(
+                            enlargeCenterPage: true,
+                            height: 700,
+                            initialPage: 0,
+                            scrollDirection: Axis.horizontal,
+                            autoPlayInterval:
+                                const Duration(milliseconds: 5000),
+                            autoPlay: false,
+                          )),
+                    ),
+                    SizedBox(
+                      height: 40.h,
+                    ),
+                    Container(
+                      width: size.width * 0.8,
+                      child: Text(
+                        "So, whether you're a traveler at heart or just looking for a fun way to pass the time, let GeoTale take you on an unforgettable journey.",
+                        style: googleTextStyle(
+                            22.sp, FontWeight.w500, Colors.cyan),
+                        overflow: TextOverflow.clip,
+                        textAlign: TextAlign.justify,
+                      ),
+                    ),
                   ],
                 ),
               ),
